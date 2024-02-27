f2ab48c05e7596116c25081fb5f0ebc0b4015283
Kyle McColgan <kdmbus21@gmail.com>
2024-02-26 20:57:14 -0600

    Update receiveMessage() in rover.js

--- rover.js
+++ rover.js
@@ -1,19 +1,32 @@
 class Rover {
-   // Write code here!
-   constructor(mode = 'NORMAL', generatorWatts = 110) {
-     this.mode = mode;
-     // if (!mode) {
-     //   throw Error("Rover mode required.");
-     // }
-     this.generatorWatts = generatorWatts;
-   }
+// Write code here!
+constructor(mode = 'NORMAL', generatorWatts = 110, position)
+{
+    this.mode = mode;
+    this.position=position;
+    // if (!mode) {
+    //   throw Error("Rover mode required.");
+    // }
+    this.generatorWatts = generatorWatts;
+}
 
-   receiveMessage(message)
-   {
-      // return `message: ${message.name} + results: [${message.commands}]`;
+    receiveMessage(message)
+    {
+        // return `message: ${message.name} + results: [${message.commands}]`;
+        // let results = message.commands;
+        // results.push({mode:this.mode, generatorWatts:this.generatorWatts, position:this.position})
+        let result;
+        if (message.commands[0].commandType == 'STATUS_CHECK')
+        {
+            result = {message:message.name, results:message.commands};
+        }
+        else
+        {
+            result = {message:message.name, results:message.commands};
+        }
 
-      return {message:message.name, results:message.commands};
-   }
+        return result;
+    }
 }
 
 module.exports = Rover;
