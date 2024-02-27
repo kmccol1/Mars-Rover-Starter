fd045d9466f08d4e4c7be229c840afbe7fc1c7f1
Kyle McColgan <kdmbus21@gmail.com>
2024-02-26 19:44:01 -0600

    Fix tests #8-11

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -15,14 +15,24 @@ describe("Rover class", function() {
     expect(testRover.generatorWatts).toBe(110);
   });
 
-  it("response returned by receiveMessage contains the name of the message", function() {
-    let testRover = new Rover();
-    expect(testRover.receiveMessage(new Message('testName', ['testCommand1', 'testCommand2']))).toMatchObject({message:'testName'});
-  });
+   it("response returned by receiveMessage contains the name of the message", function() {
+     let testRover = new Rover();
+     expect(testRover.receiveMessage(new Message('testName', [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')]))).toMatchObject({message:'testName',results:[new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')] });
+   });
 
   it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
     let testRover = new Rover();
-    expect(testRover.receiveMessage(new Message('testName', ['testCommand1', 'testCommand2']))).toMatchObject({message:'testName',results:['testCommand1', 'testCommand2'] });
+    expect(testRover.receiveMessage(new Message('testName', [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')]))).toMatchObject({message:'testName',results:[new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')] });
+  });
+
+  it("responds correctly to the status check command", function() {
+    let testRover = new Rover();
+    expect(testRover.receiveMessage(new Message('testName', [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK'), {mode:'LOW_POWER', generatorWatts:110, position: 98382}]))).toMatchObject({message:'testName',results:[new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK'), {mode:'LOW_POWER', generatorWatts:110, position: 98382}] });
+  });
+
+  it("responds correctly to the mode change command", function() {
+    let testRover = new Rover();
+    expect(testRover.receiveMessage(new Message('testName', [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK'), {mode:'LOW_POWER', generatorWatts:110, position: 98382}]))).toMatchObject({message:'testName',results:[new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK'), {mode:'LOW_POWER', generatorWatts:110, position: 98382}] });
   });
 
 });
